8bd744e2f7fd9afac6f8edd91c53ef043d3e58a5
YK <cool_strippin@i.softbank.jp>
2019-11-16 23:26:51 +0900

    update vscode settings

--- .vscode/vscode_restore.sh
+++ .vscode/vscode_restore.sh
@@ -8,9 +8,9 @@ ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 rm "$VSCODE_SETTING_DIR/keybindings.json"
 ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
-cat ./extensions | while read line
+cat $SCRIPT_DIR/extensions | while read line
 do
  code --install-extension $line
 done
 
-code --list-extensions > extensions
+code --list-extensions > $SCRIPT_DIR/extensions
