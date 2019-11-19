65a810ac6aee68a55bf400ddae8a83e37d03d951
YK <cool_strippin@i.softbank.jp>
2019-11-20 03:13:44 +0900

    add fortune & cowsay

---
+++ fortune_cowsay.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+function getFortuneVersion() {
+  eval $1=`brew list --versions | grep fortune | sed 's/fortune //g'`
+}
+
+function getCowsayVersion() {
+  eval $1=`brew list --versions | grep cowsay | sed 's/cowsay //g'`
+}
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+
+getFortuneVersion FORTUNE_VERSION
+getCowsayVersion COWSAY_VERSION
+
+cp -i $SCRIPT_DIR/.fortune/meigen "/usr/local/Cellar/fortune/${FORTUNE_VERSION}/share/games/fortunes"
+cp -i $SCRIPT_DIR/.fortune/meigen.dat "/usr/local/Cellar/fortune/${FORTUNE_VERSION}/share/games/fortunes"
+cp -i $SCRIPT_DIR/.cowsay/lain.cow "/usr/local/Cellar/cowsay/${COWSAY_VERSION}/share/cows"
+cp -i $SCRIPT_DIR/.cowsay/mojo.cow "/usr/local/Cellar/cowsay/${COWSAY_VERSION}/share/cows"
