0f2c8ecbf90b64db6639bb29b823de973589e102
mogimogitomato <osakana.yano49@gmail.com>
2019-11-20 12:39:51 +0900

    update dotfile settings

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # dotfiles
+
+- loading dotfile under the root folder    
+.bash_alias  
+.bash_functions  
+.bashrc  
+.gitconfig  
+.hyper.js  
+.vimrc  
+.zshalias  
+.zshenv  
+.zshrc  
+→ `sh make_symlink.sh`  
+other dotfiles  
+→ please set manually  
+
+- add cowsay's image, and fortune's japanese text(for Mac)  
+→ `sh fortune_cowsay.sh`  
+
+- loading vscode settings  
+→ `sh .vscode/vscode_restore.sh`  
---
+++ make_symlink.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DOT_FILES=( .bash_alias .bash_functions .bashrc .gitconfig .hyper.js .vimrc .zshalias .zshenv .zshrc)
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+ORG_DIR=$HOME/dotfile_org
+
+for file in ${DOT_FILES[@]}
+do
+  if [ -a $HOME/$file ]; then
+    mkdir -p $ORG_DIR
+    mv -i $HOME/$file $ORG_DIR/$file
+    echo "move original file: $ORG_DIR/$file"
+  fi
+  ln -s $SCRIPT_DIR/$file $HOME/$file
+  echo "add symlink: $file"
+done
